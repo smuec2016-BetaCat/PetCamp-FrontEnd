c6806260f06afcb320f49d1539a2055ff13c5c87
weekbin <18717744742@163.com>
2018-11-24 18:34:09 +0800

    Components PetSupplies v1.2

--- src/components/FrontPage/FrontPagePetSupplies.vue
+++ src/components/FrontPage/FrontPagePetSupplies.vue
@@ -1,22 +1,22 @@
 <template>
 	<el-row>
 		<el-col :span="20" :offset="2" id="PetSide">
-			<i class="el-icon-arrow-left"></i>
 			<span id="PetSide1">特色推荐</span>
-			<i class="el-icon-arrow-right"></i>
 			<br>
 			<span id="PetSide2">宠物周边</span>
 		</el-col>
 		<el-col :span="20" :offset="2">
 			<el-row>
-				<el-col :xs={span:12,offset:6} :sm={span:8,offset:3} :md={span:6,offset:0} v-for="item in 8" :key="item">
-					<el-button @click="goTo">
+				<el-col :xs={span:12,offset:6} :sm={span:8,offset:3} :md={span:6,offset:0} v-for="item in PetSide" :key="item.id">
+					<el-button @click="goTo" @mouseenter.native="textChange(item)" @mouseleave.native="textChange(item)">
 						<el-card :body-style="{ padding: '10px' }" shadow="hover">
 							<img src="../../assets/petSupplies.jpg" class="image">
 							<div style="padding: 14px;">
-								<span>铲屎官道具</span>
-								<div class="bottom clearfix">
-									<time class="time">{{ currentDate }}</time>
+								<span style="color:#000" v-text="item.title"></span>
+								<div class="bottom clearfix" style="background-color: #f8f8fa">
+									<span v-if="item.show" v-text="item.price" style="color: #555;text-decoration: line-through"></span>
+									<span v-if="item.show" v-text="item.dprice" style="color: #555;margin-left: 10%;"></span>
+									<span v-if="!item.show" style="color: #555;">加入购物车</span>
 								</div>
 							</div>
 						</el-card>
@@ -32,7 +32,18 @@
         name: "FrontPagePetSupplies",
         data() {
             return {
-                currentDate: new Date(),
+                price:"$100.00",
+				dprice:"$80.00",
+				PetSide:[
+					{id:0,title:"铲屎官道具",price: "$100.00",dprice:"$80.00",show:true,src:""},
+					{id:1,title:"到底能不能遍历啊",price: "$150.00",dprice:"$130.00",show:true,src:""},
+					{id:2,title:"终于可以遍历了",price: "$250.00",dprice:"$130.00",show:true,src:""},
+					{id:3,title:"能不能有个悬浮效果啊",price: "$1500.00",dprice:"$1030.00",show:true,src:""},
+					{id:4,title:"用数组表示布尔值可能有点问题",price: "$1250.00",dprice:"$730.00",show:true,src:""},
+					{id:5,title:"最终决定直接尝试json文件",price: "$150.00",dprice:"$130.00",show:true,src:""},
+					{id:6,title:"突然发现就可以了",price: "$650.00",dprice:"$630.00",show:true,src:""},
+					{id:7,title:"可以只买单个物品了",price: "$1020.00",dprice:"$930.00",show:true,src:""}
+				],
                 tableData: [{
                     date: '2016-05-02',
                     name: '王小虎',
@@ -55,7 +66,10 @@
         methods:{
             goTo(){
                 alert("haha")
-            }
+            },
+			textChange(item){
+				item.show = !item.show
+			}
         }
     }
 </script>
@@ -98,11 +112,27 @@
 	margin: 0;
 	font-family:'Poppins', sans-serif;
 	font-size: 14px;
+	position: relative;
+	display: block;
+}
+#PetSide #PetSide1:before,#PetSide #PetSide1:after{
+	content: "";
+	position: absolute;
+	background: #abd373;
+	width: 5%;
+	height: 1px;
+	top: 50%;
+}
+#PetSide #PetSide1:before{
+	left: 40%;
+}
+#PetSide #PetSide1:after{
+	right: 40%;
 }
 #PetSide #PetSide2{
 	margin: 0;
 	font-family: 'Yesteryear', cursive;
-	font-size: 35px;
+	font-size: 30px;
 	display: block;
 }
 
