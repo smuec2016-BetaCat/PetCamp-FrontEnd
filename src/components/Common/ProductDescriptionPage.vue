a848c91fae4d2fcc328394814b78ba29fc255172
weekbin <18717744742@163.com>
2018-11-25 22:57:20 +0800

    V2.1.1 版本分支

--- src/components/Common/ProductDescriptionPage.vue
+++ src/components/Common/ProductDescriptionPage.vue
@@ -1,20 +1,8 @@
 <template>
-	<!--<el-row>-->
-		<!--<el-col :span="20" :offset="2">-->
-			<!--<el-col :span="6">-->
-				<!--<el-carousel indicator-position="outside">-->
-					<!--<el-carousel-item v-for="item in 4" :key="item">-->
-						<!--<h3><img src="../../assets/banner1.jpg" alt=""></h3>-->
-					<!--</el-carousel-item>-->
-				<!--</el-carousel>-->
-			<!--</el-col>-->
-			<!--<el-col :span="18">-->
-				<!--<pic-zoom url="static/imac2.jpg" :scale="3"></pic-zoom>-->
-			<!--</el-col>-->
-		<!--</el-col>-->
-	<!--</el-row>-->
-	<div class="pic-box" style="width: 500px;height: 500px;">
-		<pic-zoom url="static/imac2.jpg" :scale="3"></pic-zoom>
+	<div style="margin-left: 8.333333%">
+		<div id="zoom">
+			<pic-zoom url="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/230px-Cat03.jpg" :scale="2"></pic-zoom>
+		</div>
 	</div>
 </template>
 
@@ -27,5 +15,8 @@ export default {
 </script>
 
 <style scoped>
-
+#zoom{
+	width: 400px;
+	height: 400px;
+}
 </style>
--- src/components/FrontPage/FrontPageCarouselMap.vue
+++ src/components/FrontPage/FrontPageCarouselMap.vue
@@ -45,19 +45,19 @@ export default {
 </script>
 
 <style scoped>
-	.el-carousel__item h3 {
-		color: #475669;
-		font-size: 18px;
-		opacity: 0.75;
-		line-height: 300px;
-		margin: 0;
-	}
+.el-carousel__item h3 {
+	color: #475669;
+	font-size: 18px;
+	opacity: 0.75;
+	line-height: 300px;
+	margin: 0;
+}
 
-	.el-carousel__item:nth-child(2n) {
-		background-color: #99a9bf;
-	}
+.el-carousel__item:nth-child(2n) {
+	background-color: #99a9bf;
+}
 
-	.el-carousel__item:nth-child(2n+1) {
-		background-color: #d3dce6;
-	}
+.el-carousel__item:nth-child(2n+1) {
+	background-color: #d3dce6;
+}
 </style>
